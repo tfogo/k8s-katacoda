151834800d7cc412c74bf78592d9c26fd47046ac
Tim Fogarty <tim@tfogo.com>
2018-09-10 16:54:23 -0700

    Fix kubectl setup

--- k8s/kubectl-setup.sh
+++ k8s/kubectl-setup.sh
@@ -4,4 +4,6 @@ if [ $(hostname) == "node01" ]
 then
   mkdir $HOME/.kube
   cp /etc/kubernetes/kubelet.conf $HOME/.kube/config
+else
+  kubectl create clusterrolebinding node-admin-binding --clusterrole=cluster-admin --user=system:node:node01
 fi
